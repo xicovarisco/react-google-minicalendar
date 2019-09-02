833690107635cd4a8904a8f288edaf25c1bca23b
Francisco Varisco <francisco.varisco@gmail.com>
2019-09-02 21:33:44 +1000

    Transforming component into a React Component that renders some dummy content

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class GoogleMiniCalendar extends Component {
+    render() {
+        return (
+            <div>I am a dummy Calendar component</div>
+        )
+    }
+}
